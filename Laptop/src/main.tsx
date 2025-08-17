dcbd3716460d3954e54c46e34de0b8d5a5cc56e5
AlphaCupcake10 <alphacupcake@outlook.com>
2025-08-17 17:16:35 +0530

    Enhance loading experience with a spinner and fade-out effect; refactor main.tsx to use Suspense for loading state

--- Laptop/src/main.tsx
+++ Laptop/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -6,8 +6,18 @@ import { Canvas } from '@react-three/fiber';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Canvas camera={{ fov: 55}}>
-      <App />
-    </Canvas>
+    <Suspense fallback={<div style={{
+      background: `linear-gradient(138deg,rgba(0, 0, 0, 1) 0%, rgba(32, 36, 51, 1) 100%)`,
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#fff',
+      textAlign: 'center',
+    }}>Loading...</div>}>
+      <Canvas camera={{ fov: 55}}>
+        <App />
+      </Canvas>
+    </Suspense>
   </React.StrictMode>,
 )
